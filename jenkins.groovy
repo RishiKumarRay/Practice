6ecac3d64f4218891c77747b3ecdae472986f5ae
RishiKumarRay <rishi.ray@knoldus.com>
2021-10-08 12:55:29 +0530

    First commit

---
+++ jenkins.groovy
@@ -0,0 +1,29 @@
+pipeline { 
+  
+   agent none
+
+   stages {
+   
+     stage('Install Dependencies') { 
+         agent {label 'slave'}
+        steps { 
+           sh 'npm install' 
+        }
+     }
+     
+     stage('Test') { 
+        steps { 
+           sh 'echo "testing application..."'
+        }
+      }
+
+         stage("Deploy application") { 
+         steps { 
+           sh 'echo "deploying application..."'
+         }
+
+     }
+  
+   	}
+
+   }
